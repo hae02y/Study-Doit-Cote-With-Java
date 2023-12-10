60c96878cb092fb4fcd4321b1f70c8d5bb1cda1d
hae02y <godud1118@gmail.com>
2023-12-11 00:27:37 +0900

    feat: 004풀이

--- doitcote/lib/src/main/java/doitcote/B004_구간합구하기2.java
+++ doitcote/lib/src/main/java/doitcote/B004_구간합구하기2.java
@@ -1,9 +1,16 @@
 package doitcote;
 
+import java.util.Scanner;
+
 public class B004_구간합구하기2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); //표의 크기 (n x n)
+		int m = sc.nextInt(); //합을 구할 횟수
 
 	}
 
