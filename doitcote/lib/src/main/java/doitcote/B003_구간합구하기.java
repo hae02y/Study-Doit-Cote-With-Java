314a37e1c42e27a09ea95f4b77bac2532c66cb05
hae02y <godud1118@gmail.com>
2023-12-11 00:35:50 +0900

    feat: 노트북

--- doitcote/lib/src/main/java/doitcote/B003_구간합구하기.java
+++ doitcote/lib/src/main/java/doitcote/B003_구간합구하기.java
@@ -2,4 +2,7 @@ package doitcote;
 
 public class B003_구간합구하기 {
 
+	public static void main(String[] args) {
+		System.out.println("main");
+	}
 }
---
+++ doitcote/lib/src/main/java/doitcote/BTest추가.java
@@ -0,0 +1,8 @@
+package doitcote;
+
+public class BTest추가 {
+
+	public static void main(String[] args) {
+		System.out.println("main메서드 테스트");
+	}
+}
