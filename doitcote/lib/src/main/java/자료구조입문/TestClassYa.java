2b80b4a67291891f7bd9d4bc742c18ae38174539
hae02y <godud1118@gmail.com>
2023-12-16 21:56:12 +0900

    추가

---
+++ doitcote/lib/src/main/java/자료구조입문/TestClassYa.java
@@ -0,0 +1,12 @@
+package 자료구조입문;
+
+public class TestClassYa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("자료구조 입문 테스트");
+
+	}
+
+}
