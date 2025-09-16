32c1711e75480e1b16e4f75071e31ceea98e3537
hae02y <godud1118@gmail.com>
2025-09-16 23:09:25 +0900

    f

--- doitcote/lib/src/main/java/doitcote/BTest추가.java
+++ doitcote/lib/src/main/java/doitcote/BTest추가.java
@@ -1,8 +1,19 @@
 package doitcote;
 
+import java.util.Scanner;
+
 public class BTest추가 {
 
 	public static void main(String[] args) {
-		System.out.println("main메서드 테스트");
-	}
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int answer = 0;
+        
+        for(int i=0; i<number; i++){
+            answer += number % 100;
+            number /= 100;
+        }
+
+        System.out.println(answer);
+    }
 }
